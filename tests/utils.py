0fcc467d476f557db29ec6fdd40cfd1ed1941e49
kramber <kramber268@gmail.com>
2024-07-21 19:15:24 +0300

    feat: Add consts and function for getting nginx config

--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,11 @@
 from collections.abc import Iterator
 from pathlib import Path
 
+from docker.models.containers import Container, ExecResult
+
 NGINX_DIRECTORY: Path = Path(__file__).parent.parent / "nginx"
+IMAGE_TAG: str = "nginx-test-image"
+CONTAINER_NAME: str = "nginx-test"
 
 
 def str_from_logs(logs: Iterator[dict[str, str]]) -> str:
@@ -11,3 +15,9 @@ def str_from_logs(logs: Iterator[dict[str, str]]) -> str:
         log_string += str(log[next(iter(log.keys()))])
 
     return log_string
+
+
+def get_nginx_configuration(container: Container) -> str:
+    result: ExecResult = container.exec_run("nginx -T")
+
+    return str(result.output.decode("utf-8"))
