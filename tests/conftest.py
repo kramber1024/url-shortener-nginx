640623070acb00293bcfd2ffa3e5e39232f44048
kramber <kramber268@gmail.com>
2024-07-21 19:29:55 +0300

    feat: Add fixture for creating image

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,15 @@
+from collections.abc import Generator
+from typing import TYPE_CHECKING
+
 import docker
 import pytest
 from docker import DockerClient
+from docker.models.images import Image
+
+from tests.utils import CONTAINER_NAME, IMAGE_TAG, NGINX_DIRECTORY
+
+if TYPE_CHECKING:
+    from docker.models.containers import Container
 
 
 @pytest.fixture(scope="session")
@@ -8,3 +17,25 @@ def client() -> DockerClient:
     client: DockerClient = docker.from_env()
 
     return client
+
+
+@pytest.fixture
+def image(client: DockerClient) -> Generator[Image, None, None]:
+    image, _ = client.images.build(
+        path=str(NGINX_DIRECTORY),
+        dockerfile="Dockerfile",
+        rm=True,
+        forcerm=True,
+        tag=IMAGE_TAG,
+    )
+
+    yield image
+
+    try:
+        container: Container = client.containers.get(CONTAINER_NAME)
+        container.stop()
+        container.remove(force=True)
+    except docker.errors.NotFound:
+        pass
+
+    image.remove(force=True)
