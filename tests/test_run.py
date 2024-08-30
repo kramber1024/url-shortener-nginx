50ca75e4fe21290cfbb73ccbcc221bd306ef9521
kramber1024 <kramber268@gmail.com>
2024-08-30 13:14:22 +0300

    feat: Update tests for new configuration

--- tests/test_run.py
+++ tests/test_run.py
@@ -10,6 +10,8 @@ from tests.utils import CONTAINER_NAME, get_nginx_configuration
 
 
 def test_run(client: DockerClient, image: Image) -> None:
+    api_server_host: str = "127.0.0.1"
+    api_server_port: str = "26801"
     web_server_host: str = "127.0.0.1"
     web_server_port: str = "26803"
 
@@ -23,6 +25,8 @@ def test_run(client: DockerClient, image: Image) -> None:
         environment={
             "WEB_SERVER_HOST": web_server_host,
             "WEB_SERVER_PORT": web_server_port,
+            "API_SERVER_HOST": api_server_host,
+            "API_SERVER_PORT": api_server_port,
         },
         detach=True,
     )
@@ -34,23 +38,34 @@ def test_run(client: DockerClient, image: Image) -> None:
     assert "test is successful" in nginx_configuration
     assert "server_name nginx;" in nginx_configuration
     assert "server_tokens off;" in nginx_configuration
+    assert f"{api_server_host}:{api_server_port}" in nginx_configuration
     assert f"{web_server_host}:{web_server_port}" in nginx_configuration
     assert "${" not in nginx_configuration
 
     attrs: dict[str, Any] = container.attrs
-    assert attrs.get("Path") == "/usr/local/bin/launch.sh"
-    assert attrs.get("Args") == [
-        "nginx",
-        "-g",
-        "daemon off;",
-    ]
+    assert attrs.get("Path") == "/usr/local/bin/entrypoint.sh"
+    assert attrs.get("Args") == []
     assert attrs.get("Platform") == "linux"
-    assert attrs.get("HostConfig", {}).get(
-        "PortBindings",
-    ).get("443/tcp")[0].get("HostPort") == "443"
-    assert attrs.get("HostConfig", {}).get(
-        "PortBindings",
-    ).get("80/tcp")[0].get("HostPort") == "80"
+    assert (
+        attrs.get("HostConfig", {})
+        .get(
+            "PortBindings",
+        )
+        .get("443/tcp")[0]
+        .get("HostPort")
+        == "443"
+    )
+    assert (
+        attrs.get("HostConfig", {})
+        .get(
+            "PortBindings",
+        )
+        .get("80/tcp")[0]
+        .get("HostPort")
+        == "80"
+    )
+    assert f"API_SERVER_HOST={api_server_host}" in attrs.get("Config", {}).get("Env")
+    assert f"API_SERVER_PORT={api_server_port}" in attrs.get("Config", {}).get("Env")
     assert f"WEB_SERVER_HOST={web_server_host}" in attrs.get("Config", {}).get("Env")
     assert f"WEB_SERVER_PORT={web_server_port}" in attrs.get("Config", {}).get("Env")
 
